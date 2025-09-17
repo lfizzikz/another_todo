10a5df1e903a5ca6d28891f6b9974cc7ff6e86eb
Trevor Nance <trevornance7@gmail.com>
2025-09-17 12:55:30 -0400

    tasks now show in homepage, rather than separate page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 import _sqlite3
-from flask import Flask, g, redirect, render_template, request
+from flask import Flask, g, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -10,11 +10,12 @@ TASKS = []
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", tasks=TASKS)
 
 
 @app.route("/add", methods=["POST"])
 def add():
-    task = request.form["task"]
-    TASKS.append(task)
-    return render_template("add.html", tasks=TASKS)
+    task_name = request.form.get("task")
+    if task_name:
+        TASKS.append(task_name)
+        return redirect(url_for("index"))
